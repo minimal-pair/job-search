67adab79b125f8f799f49ff1a79d88ec3b5977c1
Tim Austin <tja.xpprime@gmail.com>
2023-11-10 15:15:01 -0700

    End section 23

--- src/components/Navigation/MainNav.vue
+++ src/components/Navigation/MainNav.vue
@@ -46,7 +46,7 @@ export default {
   data() {
     return {
       menuItems: [
-        { text: "Teams", url: "/" },
+        { text: "Teams", url: "/teams" },
         { text: "Locations", url: "/" },
         { text: "Life at Timbo Careers", url: "/" },
         { text: "How we hire", url: "/" },
